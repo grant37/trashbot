e613e033ab631ed5294a91a29629b199a780e43b
jetkin01 <joshetkind@yahoo.com>
2018-04-30 16:10:24 -0400

    Update prototype_candidate_recognition_segmentation.cpp

--- pointcloud_object/prototype_candidate_recognition_segmentation.cpp
+++ pointcloud_object/prototype_candidate_recognition_segmentation.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <tf/transform_listener.h>
@@ -6,6 +7,11 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 
 tf::TransformListener listener;
@@ -15,6 +21,11 @@ PointCloudT::Ptr cloud (new PointCloudT);
 //Variables for cloudPrep
 bool tranformed = 0;
 
+/*
+cloudPrep is callback of pointcloud topic subscriber 
+Takes pointcloud in camera frame of reference, transforms to baselink frame, and converts to PCL format
+Stores prepared cloud in global cloud variable
+*/
 void cloudPrep(const  sensor_msgs::PointCloud2ConstPtr& input)
 {
 	//Creating container for transformedCloud
@@ -22,13 +33,57 @@ void cloudPrep(const  sensor_msgs::PointCloud2ConstPtr& input)
 
 
 	//Transform to baselink frame
-	transformed = pcl_ros::transformPointCloud(&"***BaselinkFrame***", &input, &transformedCloud, &listener);
+	transformed = pcl_ros::transformPointCloud(&"/baselink", &input, &transformedCloud, &listener);
 	//error catching transform
-	std::cerr<<"Transformed?: " << transformed << endl;
-
+	//std::cerr<<"Transformed?: " << transformed << endl;
 
   	//Convert to PCL format
 	pcl::fromROSMsg(*transformedCloud, *cloud);
+}
+
+
+void planeSegmenting(double distThresh){
+
+	//Maybe z-dir pass thru filter to get rid of points higher than some threshold height?
+
+	
+	//From PCL planar segmentation tutorial http://pointclouds.org/documentation/tutorials/planar_segmentation.php
+	  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+	  // Create the segmentation object
+	  pcl::SACSegmentation<pcl::PointXYZ> seg;
+	  // Optional
+	  seg.setOptimizeCoefficients (true);
+	  // Mandatory
+	  seg.setModelType (pcl::SACMODEL_PLANE);
+	  seg.setMethodType (pcl::SAC_RANSAC);
+	  seg.setDistanceThreshold (distThresh);
+
+	  //look for a plane perpendicular to a given axis
+	  seg.setModelType (pcl::SACMODEL_PERPENDICULAR_PLANE);
+	  seg.setMethodType (pcl::SAC_RANSAC);
+	  seg.setMaxIterations (1000);
+	  seg.setDistanceThreshold (0.01);
+
+	  seg.setInputCloud (cloud);
+	  seg.segment (*inliers, *coefficients);
+
+	  if (inliers->indices.size () == 0)
+	  {
+	    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+	    return (-1);
+	  }
+
+	  //How to get model coefficients out? How will we use them? 
+
+	  //Outputing for debugging
+	  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+
+	  //Is this the largest plane? Will it always detect the ground plane?
 }
 
 int main(int argc, char **argv)
@@ -37,8 +92,9 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("***PointCloudPublishingTopic***", 1000, cloudPrep);
+  ros::Subscriber sub = n.subscribe("/camera/depth/points", 1, cloudPrep);//keeps 1 cloud in queue to always get most recent
 
+  ros::ServiceServer getCandidates_service = n.advertiseService("getCandidates", planeSegmenting);
 
   ros::spin();
 
