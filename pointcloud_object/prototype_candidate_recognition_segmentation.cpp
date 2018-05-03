9118945ab74fbd1265a0cb5826b4cf34eb9e402d
jetkin01 <joshetkind@yahoo.com>
2018-05-02 20:06:41 -0400

    Update prototype_candidate_recognition_segmentation.cpp

--- pointcloud_object/prototype_candidate_recognition_segmentation.cpp
+++ pointcloud_object/prototype_candidate_recognition_segmentation.cpp
@@ -2,8 +2,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <tf/transform_listener.h>
-// PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
+// PCL specific includes
+#include <pcl_ros/impl/transforms.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -13,37 +14,20 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+//template <typename PointT> class PointCloud;
+
+typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointXYZ PointX;
+typedef pcl::PointCloud<PointT> PointCloudT;
+typedef pcl::PointCloud<PointX> PointCloudX;
 
 tf::TransformListener listener;
 
-PointCloudT::Ptr cloud (new PointCloudT);
+PointCloudX::Ptr cloud (new PointCloudX);
 
-//Variables for cloudPrep
-bool tranformed = 0;
+void planeSegmenting(){
 
-/*
-cloudPrep is callback of pointcloud topic subscriber 
-Takes pointcloud in camera frame of reference, transforms to baselink frame, and converts to PCL format
-Stores prepared cloud in global cloud variable
-*/
-void cloudPrep(const  sensor_msgs::PointCloud2ConstPtr& input)
-{
-	//Creating container for transformedCloud
-	sensor_msgs::PointCloud2 transformedCloud
-
-
-	//Transform to baselink frame
-	transformed = pcl_ros::transformPointCloud(&"/baselink", &input, &transformedCloud, &listener);
-	//error catching transform
-	//std::cerr<<"Transformed?: " << transformed << endl;
-
-  	//Convert to PCL format
-	pcl::fromROSMsg(*transformedCloud, *cloud);
-}
-
-
-void planeSegmenting(double distThresh){
-
+         double distThresh = 10;
 	//Maybe z-dir pass thru filter to get rid of points higher than some threshold height?
 
 	
@@ -64,14 +48,13 @@ void planeSegmenting(double distThresh){
 	  seg.setMethodType (pcl::SAC_RANSAC);
 	  seg.setMaxIterations (1000);
 	  seg.setDistanceThreshold (0.01);
-
+                //cloud needs to by xyz not xyzrgb
 	  seg.setInputCloud (cloud);
 	  seg.segment (*inliers, *coefficients);
 
 	  if (inliers->indices.size () == 0)
 	  {
 	    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-	    return (-1);
 	  }
 
 	  //How to get model coefficients out? How will we use them? 
@@ -86,6 +69,35 @@ void planeSegmenting(double distThresh){
 	  //Is this the largest plane? Will it always detect the ground plane?
 }
 
+/*
+cloudPrep is callback of pointcloud topic subscriber 
+Takes pointcloud in camera frame of reference, transforms to baselink frame, and converts to PCL format
+Stores prepared cloud in global cloud variable
+*/
+void cloudPrep(const  sensor_msgs::PointCloud2ConstPtr& input)
+{
+        //Convert to PCL format
+        PointCloudT inputT;
+	pcl::fromROSMsg(*input, inputT);     
+           
+        //Creating container for transformedCloud
+	PointCloudT transformedCloud;
+
+        //destination frame
+        const std::string destFrame = "/baselink";
+        
+	//Transform to baselink frame
+	bool transformed = pcl_ros::transformPointCloud<PointT>(destFrame, inputT, transformedCloud, listener);
+        //http://docs.ros.org/kinetic/api/pcl_ros/html/namespacepcl__ros.html#aad1ce4ad90ab784aae6158419ad54d5f
+        
+	//error catching transform
+	std::cerr<<"Transformed?: " << transformed << std::endl;
+        
+        planeSegmenting();
+  	
+}
+
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "candidate_detection");
@@ -93,8 +105,6 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/camera/depth/points", 1, cloudPrep);//keeps 1 cloud in queue to always get most recent
-
-  ros::ServiceServer getCandidates_service = n.advertiseService("getCandidates", planeSegmenting);
 
   ros::spin();
 
