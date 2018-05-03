2d083b2ae488254b0054f9833016b6c1f52e23db
grant37 <grant.sisson@tufts.edu>
2018-05-03 13:44:45 -0400

    node to be tested for trash detection

---
+++ opencv_workspace/trash_detector_node.py
@@ -0,0 +1,62 @@
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from copy import deepcopy
+
+TRASH_CLASSIFIER = cv2.CascadeClassifier('cups.xml')
+
+class trashDetector(object):
+
+	def __init__(self):
+		rospy.init_node("trash_detector_node", anonymous=True)
+		self.trash_status_pub = rospy.Publisher('trashbot/status', String, queue_size = 1)
+
+		self.bridge_object = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_callback)
+		self.frame = None
+
+		# cascade
+		self.cups_cascade = cv2.CascadeClassifier("/trashbot/opencv_workspace/cups_cascade.xml")
+
+	def image_callback(self, data):
+		try:
+			#bgr8 is default opencv encoding
+			self.frame = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+	def detect_trash(self):
+		if self.frame == None:
+			return
+
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		cups = self.cups_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20, 20))
+
+		# found trash
+		if len(cups):
+			self.trash_status_pub.publish("Found {} trash,".format(len(cups)))
+		else:
+			self.trash_status_pub.publish("Found no trash")
+
+	def run():
+		# Detector will run at 10Hz just because
+		r = rospy.Rate(10)
+		while not rospy.is_shutdown():
+			self.detect_trash() 			# check for trash
+			r.sleep()						# sleeeeeeeeeeeep
+
+
+def main():
+	trash_detector = trashDetector()
+	try:
+			trash_detector.run()
+	except KeyboardInterrupt:
+		print("Closing...")
+
+if __name__ == '__main__':
+	main()
