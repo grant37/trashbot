885023d3ae880f2a62e213ded241653e37edc658
grant37 <grant.sisson@tufts.edu>
2018-05-07 12:03:22 -0400

    cleanup pre test

--- navigation/trash/src/waypoint_navigation.cpp
+++ navigation/trash/src/waypoint_navigation.cpp
@@ -35,39 +35,26 @@ int move_turtle_bot (double x, double y, double yaw)
 	
 	std::cout<<"Going to :"<< x  << y;
 	
-	//set the header
-    	goal.target_pose.header.stamp = ros::Time::now();
-    	goal.target_pose.header.frame_id = "/map";
-	  
-    	//set relative x, y, and angle
-    	goal.target_pose.pose.position.x = x;
-    	goal.target_pose.pose.position.y = y;
-    	goal.target_pose.pose.position.z = 0.0;
-    	goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+	// set the header
+    goal.target_pose.header.stamp = ros::Time::now();
+	goal.target_pose.header.frame_id = "/map";
+  
+	// set relative x, y, and angle
+	goal.target_pose.pose.position.x = x;
+	goal.target_pose.pose.position.y = y;
+	goal.target_pose.pose.position.z = 0.0;
+	goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
 
-	//send the goal
-    	ac.sendGoal(goal);
+	// send the goal
+    ac.sendGoal(goal);
     
-    	//block until the action is completed
-    	ac.waitForResult();
-    	//std::cout<<"\n \n waiting for " << sleep_time<<" seconds \n \n";
-    	//sleep(sleep_time);
+    // block until the action is completed
+    ac.waitForResult();
+    // std::cout<<"\n \n waiting for " << sleep_time<<" seconds \n \n";
+    // sleep(sleep_time);
   
   	return 0;
 }
-
-
-////turn in a circle
-//int turn_turtle_bot(double yaw)
-//{
-//    //set new yaw goal
-//    goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw + 2*PI);
-//
-//    ac.sendGoal(goal);
-//    ac.waitForResult();
-//
-//    return 0;
-//}
 
 
 int main(int argc, char **argv)
@@ -77,26 +64,22 @@ int main(int argc, char **argv)
     
     ros::Publisher image_pub = n.advertise<sensor_msgs::Image>("camera/rgb/image_raw", 50);
 
-
-
-	//the order is:
+	// the order is:
 	// 0: right outside lab
 	// 1: right outside collaborative lounge
 	// 2: right outside kitchenette
 	// 3: close to entrnace stairs
 	
-
-//check the locations on the map to make sure they match
-	double home_location[3] = {5.65,13.8,0.0};
+	// check the locations on the map to make sure they match
+	double home_location[3] = {5.65, 13.8, 0.0};
 	
 	int num_locations = 7;
-	double locations[7][3] = { {21.7,13.7,0.0},{21.8,5.9,0.0},{-0.329,6.21,0.0},{1.0,13.6,0.0},	{5.65,13.8,0.0},{7.5,9.8,0.0},{21.3,19.5,0.0} };
+	double locations[7][3] = { {21.7,13.7,0.0}, {21.8,5.9,0.0}, {-0.329,6.21,0.0}, {1.0,13.6,0.0}, {5.65,13.8,0.0}, {7.5,9.8,0.0}, {21.3,19.5,0.0} };
 	
-	
-
 	
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base",true);
-	ac.waitForServer(); //wait to make sure the service is there
+	ac.waitForServer();
+
 	move_base_msgs::MoveBaseGoal goal;
   
 	double x1,y1,x2,y2,x3,y3,x4,y4 = 0;
@@ -109,28 +92,16 @@ int main(int argc, char **argv)
 		move_turtle_bot(locations[c][0],locations[c][1],locations[c][2]);
 		
         ros::Rate loop_rate(1.0);
+
 		for (int i = 0; i < 5; i ++){
 
 			// turn
 			move_turtle_bot(0, 0, 1.0);
-
-			// ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("cloud", 50);
             
-  			
-
-//              unsigned int num_points = 100;
-
 		 	int count = 0;
 		  	ros::Rate r(1.0);
-		  	while(n.ok()){
-                
-//                sensor_msgs::PointCloud cloud;
-//                cloud.header.stamp = ros::Time::now();
-//                cloud.header.frame_id = "sensor_frame";
-//
-//                cloud.points.resize(num_points);
-//
-//                cloud_pub.publish(cloud);
+
+		  	while(n.ok()) {
                 
                 sensor_msgs::Image view;
                 view.header.stamp = ros::Time::now();
@@ -146,7 +117,6 @@ int main(int argc, char **argv)
         }
 		
 		sleepok(10,n);
-
 		
 		// increment location
 		c++;
