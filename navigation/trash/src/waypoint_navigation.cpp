9765efdd52b8d069303dade6ba6d980706e85fb1
grant37 <grant.sisson@tufts.edu>
2018-05-08 15:11:10 -0400

    debugged with weak test case and cleaned up a little

--- navigation/trash/src/waypoint_navigation.cpp
+++ navigation/trash/src/waypoint_navigation.cpp
@@ -62,27 +62,29 @@ int move_turtle_bot (double x, double y, double yaw, bool turn)
   	return 0;
 }
 
+
+// the order is:
+// 0: right outside lab
+// 1: right outside collaborative lounge
+// 2: right outside kitchenette
+// 3: close to entrnace stairs
+
 int main(int argc, char **argv)
 {
 	
 	std::cerr << "starting up..." << std::endl;
 	ros::init(argc, argv, "move_base_client");
 	ros::NodeHandle n;
-    
-    // ros::Publisher image_pub = n.advertise<sensor_msgs::Image>("camera/rgb/image_raw", 50);
-	// std::cerr << "Publishing image..." << std::endl;
+
+	std_msgs::Bool search;
+	search.data = false;
 
 	ros::Publisher init_check = n.advertise<std_msgs::Bool>("search_with_cv", 50);
 
-
-	// the order is:
-	// 0: right outside lab
-	// 1: right outside collaborative lounge
-	// 2: right outside kitchenette
-	// 3: close to entrnace stairs
+	// don't look for trash while wandering the halls
+	init_check.publish(search);
 	
-
-	//check the locations on the map to make sure they match
+	// Note! check the locations on the map to make sure they match
 	double home_location[3] = {12.70,24.27,0.0};
 	
 	int num_locations = 2;
@@ -92,8 +94,7 @@ int main(int argc, char **argv)
 	ac.waitForServer(); //wait to make sure the service is there
 	move_base_msgs::MoveBaseGoal goal;
   
-	double x1,y1,x2,y2,x3,y3,x4,y4 = 0;
-	
+  	// track of locations	
 	int c = 0;  
 	
 	while (ros::ok()) {
@@ -101,41 +102,28 @@ int main(int argc, char **argv)
 		// move to next location
 		// move_turtle_bot(locations[c][0],locations[c][1],locations[c][2], false);
 		
-        ros::Rate loop_rate(10);
-		for (int i = 0; i < 12; i ++){
+		// Rotate in place to search for trash
+        ros::Rate loop_rate(0.2);
+        int i = 0;
+
+		while (i < 12){
+			i++;
 			std::cerr << "rotate behavior /n" << std::endl;
-			std::cerr << i << std::endl;
-			// turn
-			move_turtle_bot(0.0, 0.0, PI/4, true);            
-  			
-		 	int count = 0;
-				     
-            // sensor_msgs::Image view;
-            // view.header.stamp = ros::Time::now();
-            // view.header.frame_id = "camera/rgb/image_raw";
-            
-            // image_pub.publish(view);
+			std::cerr << i << std::endl
 
-            ros::Time startTime = ros::Time::now();
-            ros::Duration seconds_to_spend = ros::Duration(4);
-            ros::Time endTime = seconds_to_spend + startTime;
-
-            while(ros::Time::now() < endTime) {
-            	init_check.publish(true);
-            	ros::Duration(0.1).sleep();
-            }
-
-            init_check.publish(false); // tell trash_detector to stop looking
-            
-	    	++count;
+			search.data = true;
+            init_check.publish(search); // tell trash_detector to start looking
 
             loop_rate.sleep();
         }
+
+        // reset in order to roam again
+        search.data = false;
+        init_check.publish(search);
 		
 		std::cerr << "out of while loop" << std::endl;
-		sleepok(10,n);
+		sleepok(5,n);
 		std::cerr << "out of sleepok" << std::endl;
-
 		
 		// increment location
 		c++;
